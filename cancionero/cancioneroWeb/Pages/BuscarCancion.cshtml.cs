e73b290a99a520674d49a6d7dcd994ccd160d3ca
Luis Waldman <luiswaldman@gmail.com>
2023-02-12 19:10:34 -0300

    Agregue la pagina buscar

---
+++ cancionero/cancioneroWeb/Pages/BuscarCancion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cancioneroWeb.Pages
+{
+    public class BuscarCancionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
