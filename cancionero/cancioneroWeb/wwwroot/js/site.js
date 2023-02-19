90e94ceae7470bdcaa6c37678cc59b72d7edf3e0
Luis Waldman <luiswaldman@gmail.com>
2023-02-19 16:07:10 -0300

    Comitear

--- cancionero/cancioneroWeb/wwwroot/js/site.js
+++ cancionero/cancioneroWeb/wwwroot/js/site.js
@@ -1,31 +1,54 @@
 ﻿Datos =
 {
     EditandoCancion: {
-        Nombre: "Homero",
-        Autor: "Viejas Locas",
-    }
+        Nombre: "antologia",
+        Autor: "shakira"
+    },
+    actualizando_cancion: false,
+    id_cancionseleccionada: 2,
+    CancionesDisponibles: [ 
+        { Nombre: "antologia", Autor: "shakira" },
+        { Nombre: "homero", Autor: "viejas locas" },
+        { Nombre: "gallo rojo", Autor: "fabulosos" },
+        { Nombre: "contigo", Autor: "joaquin sabina" },
+        { Nombre: "una_cancion_para_la_magdalena", Autor: "joaquin sabina" },
+        { Nombre: "flaca", Autor: "andres_calamaro" },
+        { Nombre: "paloma", Autor: "andres_calamaro" },
+        { Nombre: "ansia_en_plaza_francia", Autor: "andres_calamaro" }
+        ],
 }
 
 function ReemplazaEspaciosPor_(Cadena) {
-    return Cadena.replace(/ /g, "_");
+    return Cadena.replace(" ", "_"); //Cadena.replace(/ /g, "_");
 }
 
 ControladorBandas = {
 
-    CargarBanda: function (Nombre, Autor) {
+    CargarBanda: function (appVue) {
+
+        let temaSeleccionado = Datos.CancionesDisponibles[Datos.id_cancionseleccionada];
+
+        let Nombre = temaSeleccionado.Nombre;
+        let Autor = temaSeleccionado.Autor;
 
         bNombre = ReemplazaEspaciosPor_(Nombre);
         bAutor = ReemplazaEspaciosPor_(Autor);
         fetch("https://localhost:7073/json/" + bAutor + "/" + bNombre + ".json")
             .then(response => response.json())
             .then(data => {
-                (Datos.EditandoCancion.PartesCancion = data);
-                IniciarApp();
+                { 
+                    
+                    appVue.$forceUpdate();
+                    Datos.EditandoCancion.PartesCancion = data
+                    Datos.EditandoCancion.Nombre = Nombre;
+                    Datos.EditandoCancion.Autor = Autor;
+                    
+                }
+                    ;
             });
     }
 }
 
-ControladorBandas.CargarBanda(Datos.EditandoCancion.Nombre, Datos.EditandoCancion.Autor);
 
 const { createApp } = Vue
 
@@ -36,10 +59,22 @@ function IniciarApp() {
             return Datos;
         },
         created() {
-            // Simple GET request using fetch
+            
+            ControladorBandas.CargarBanda(this);
+        },
+        methods: {
+            Click_ActualizarCancion: function () {
+                Datos.actualizando_cancion = true;
+                this.$forceUpdate();
+            },
+            Cambio_seleccion: function () {
+                ControladorBandas.CargarBanda(this);
+            }
         }
+        
     }).mount('#app')
 
 }
 
 
+IniciarApp();
